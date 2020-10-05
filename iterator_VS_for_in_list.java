5c978f95aee05f75a2ca53619ff6028d4ef4763d
keeka2 <keeka2@naver.com>
2020-10-05 15:41:55 +0900

    iterator가 약간 빠름

---
+++ iterator_VS_for_in_list.java
@@ -0,0 +1,38 @@
+package algorithm_practice;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class iterator_VS_for_in_list {
+
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<Integer>();
+		for(int i =0;i<1000000;i++) {
+			list.add(i);
+		}
+		long beforeTime = System.currentTimeMillis();
+		int k = 0;
+		for(Integer i : list) {
+			System.out.println(i);
+			k=i;
+		}
+		long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
+		long secDiffTime1 = (afterTime - beforeTime); //두 시간에 차 계산
+	
+		
+		Iterator<Integer> it = list.iterator();
+		beforeTime = System.currentTimeMillis();
+		k = 0;
+		while(it.hasNext()) {
+			k=it.next();
+			System.out.println(k);
+		}
+		afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
+		long secDiffTime2 = (afterTime - beforeTime); //두 시간에 차 계산
+		System.out.println("시간차이(m) : "+secDiffTime1);
+		System.out.println("시간차이(m) : "+secDiffTime2);
+
+	}
+
+}
